d155fd19b12ddc435898c9aa03868d700f78aff8
BagusRezky <bagusrezky15@gmail.com>
2023-11-13 23:42:55 +0700

    [ADD] Tambah pengaturan

--- lib/Page/Admin/admin_setting.dart
+++ lib/Page/Admin/admin_setting.dart
@@ -1,18 +1,132 @@
 ///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../color/color.dart';
 
 class AdminSetting extends StatefulWidget {
-  const AdminSetting({super.key});
+  const AdminSetting({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<AdminSetting> createState() => _AdminSettingState();
 }
 
 class _AdminSettingState extends State<AdminSetting> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController nameController = TextEditingController();
+  // TextEditingController tenantController = TextEditingController();
+  // TextEditingController sekolahController = TextEditingController();
+
+  String? tokenJwt = "";
+  List<dynamic> itemsPeriode = []; // periode
+  String? selectedValuePeriode; // pe
+
+  @override
+  void initState() {
+    super.initState();
+    showToken();
+    fetchDataPeriodeFromApi();
+  }
+
+  void showToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final token = prefs.get('tokenJwt');
+    print('$token');
+  }
+
+  Future<void> fetchDataPeriodeFromApi() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? tokenJwt = prefs.getString('tokenJwt');
+    final token = 'Bearer $tokenJwt';
+    String? tenant;
+
+    Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
+
+    tenant = decodedToken['tenant_id'];
+    print('Bearer $tokenJwt');
+    final url = Uri.parse(
+        'https://jurnalmengajar-1-r8590722.deta.app/periode-cari?tenant_id=$tenant&sort_order=ascending&page=1&limit=10');
+
+    final headers = {
+      'accept': 'application/json',
+      'Authorization': token,
+    };
+
+    final response = await http.post(url, headers: headers);
+
+    if (response.statusCode == 200) {
+      var listData = jsonDecode(response.body);
+      setState(() {
+        itemsPeriode = listData['Data'];
+      });
+    } else {
+      throw Exception('Failed to load data from the API');
+    }
+  }
+
+  Future<void> createSetting() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? tokenJwt = prefs.getString('tokenJwt');
+
+    final name = nameController.text;
+    // final sekolah = sekolahController.text;
+    // final tenantId = tenantController.text;
+    final token = 'Bearer $tokenJwt';
+    print('Bearer $tokenJwt');
+    final url = Uri.parse(
+        'https://jurnalmengajar-1-r8590722.deta.app/pengaturan-tambah');
+
+    final Map<String, dynamic> body = {
+      'periode': selectedValuePeriode,
+      'batas_input': name,
+      // 'nama_sekolah': sekolah,
+      'tenant_id': '651a3ea147a3d131b32ff353',
+    };
+
+    print('Request Body: $body');
+
+    final headers = {
+      'accept': 'application/json',
+      'Authorization': token,
+    };
+
+    try {
+      final response = await http.post(url, headers: headers, body: body);
+      print('Response Code: ${response.statusCode}');
+
+      if (response.statusCode == 200) {
+        print('Pembuatan periode berhasil');
+        print('Response Body: ${response.body}');
+        Future.delayed(const Duration(seconds: 2), () {
+          setState(() {});
+          Navigator.pop(context);
+        });
+      } else if (response.statusCode == 404) {
+        print('Periode Sudah Digunakan');
+        print('Response Body: ${response.body}');
+        // Handle 404 response (data already exists)
+      } else {
+        print('Pembuatan periode gagal');
+        print('Response Body: ${response.body}');
+      } // Handle other status codes (422 or any other)
+    } catch (error) {
+      print('Error: $error');
+      // Handle network errors
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,158 +169,174 @@ class _AdminSettingState extends State<AdminSetting> {
       ),
       body: SafeArea(
         top: true,
-        child: Stack(
-          alignment: const AlignmentDirectional(0, -1),
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                  child: Align(
-                    alignment: Alignment(-0.8, 0.0),
-                    child: Text(
-                      "Periode",
-                      textAlign: TextAlign.start,
-                      overflow: TextOverflow.clip,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 16,
-                        color: Color(0xff000000),
+        child: Form(
+          key: _formKey,
+          child: Stack(
+            alignment: const AlignmentDirectional(0, -1),
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    child: Align(
+                      alignment: Alignment(-0.8, 0.0),
+                      child: Text(
+                        "Periode",
+                        textAlign: TextAlign.start,
+                        overflow: TextOverflow.clip,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 16,
+                          color: Color(0xff000000),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                        width: MediaQuery.of(context).size.width * 0.85,
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 4, horizontal: 8),
-                        decoration: BoxDecoration(
-                          color: const Color(0xfff2f2f2),
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                        child: DropdownButtonHideUnderline(
-                          child: DropdownButton(
-                            value: "Pilih Periode",
-                            items: ["Pilih Periode"]
-                                .map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
-                            style: const TextStyle(
-                              color: Color(0xff000000),
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
-                              fontStyle: FontStyle.normal,
-                            ),
-                            onChanged: (value) {},
-                            elevation: 8,
-                            isExpanded: true,
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                          width: MediaQuery.of(context).size.width * 0.85,
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 4, horizontal: 8),
+                          decoration: BoxDecoration(
+                            color: const Color(0xfff2f2f2),
+                            borderRadius: BorderRadius.circular(4),
                           ),
-                        )),
+                          child: DropdownButtonHideUnderline(
+                            child: DropdownButton(
+                              value: selectedValuePeriode,
+                              items: itemsPeriode.map((item) {
+                                return DropdownMenuItem(
+                                  value: item['nama'],
+                                  child: Text(item['nama']),
+                                );
+                              }).toList(),
+                              style: const TextStyle(
+                                color: Color(0xff000000),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.normal,
+                              ),
+                              onChanged: (value) {
+                                setState(() {
+                                  selectedValuePeriode = value as String?;
+                                });
+                              },
+                              elevation: 8,
+                              isExpanded: true,
+                            ),
+                          )),
+                    ),
                   ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  child: Align(
-                    alignment: Alignment(-0.8, 0.0),
-                    child: Text(
-                      "Batas Input Jurnal",
-                      textAlign: TextAlign.start,
-                      overflow: TextOverflow.clip,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 16,
-                        color: Color(0xff000000),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: Align(
+                      alignment: Alignment(-0.8, 0.0),
+                      child: Text(
+                        "Batas Input Jurnal",
+                        textAlign: TextAlign.start,
+                        overflow: TextOverflow.clip,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 16,
+                          color: Color(0xff000000),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                        width: MediaQuery.of(context).size.width * 0.85,
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 4, horizontal: 8),
-                        decoration: BoxDecoration(
-                          color: const Color(0xffeeeeee),
-                          borderRadius: BorderRadius.circular(4),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(25, 5, 25, 0),
+                    child: TextFormField(
+                      controller: nameController,
+                      obscureText: false,
+                      textAlign: TextAlign.start,
+                      maxLines: 1,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Wajib diisi';
+                        }
+                        return null; // Return null if validation passes
+                      },
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Poppins',
+                        fontSize: 14,
+                        color: Color(0xff000000),
+                      ),
+                      decoration: InputDecoration(
+                        disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: const BorderSide(
+                              color: Color(0x00000000), width: 1),
                         ),
-                        child: DropdownButtonHideUnderline(
-                          child: DropdownButton(
-                            value: "Pilih Batas Input",
-                            items: ["Pilih Batas Input"]
-                                .map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
-                            style: const TextStyle(
-                              color: Color(0xff000000),
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
-                              fontStyle: FontStyle.normal,
-                            ),
-                            onChanged: (value) {},
-                            elevation: 8,
-                            isExpanded: true,
-                          ),
-                        )),
-                  ),
-                ),
-              ],
-            ),
-            Align(
-              alignment: const AlignmentDirectional(0, 1),
-              child: Container(
-                margin: const EdgeInsets.only(top: 365),
-                padding: const EdgeInsets.fromLTRB(19, 22, 21, 24),
-                width: double.infinity,
-                height: 100,
-                decoration: const BoxDecoration(
-                  color: Color(0xff345ea8),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
-                ),
-                child: TextButton(
-                  // group34456KJR (4:1128)
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.zero,
-                  ),
-                  child: Container(
-                    width: double.infinity,
-                    height: double.infinity,
-                    decoration: BoxDecoration(
-                      color: const Color(0xff4ab4de),
-                      borderRadius: BorderRadius.circular(20),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: const BorderSide(
+                              color: Color(0x00000000), width: 1),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: const BorderSide(
+                              color: Color(0x00000000), width: 1),
+                        ),
+                        filled: true,
+                        fillColor: const Color(0xfff2f2f3),
+                        isDense: false,
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 10),
+                      ),
                     ),
-                    child: const Center(
-                      child: Center(
-                        child: Text(
-                          'Simpan',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            height: 1.5,
-                            color: Color(0xffffffff),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: const AlignmentDirectional(0, 1),
+                child: Container(
+                  margin: const EdgeInsets.only(top: 365),
+                  padding: const EdgeInsets.fromLTRB(19, 22, 21, 24),
+                  width: double.infinity,
+                  height: 100,
+                  decoration: const BoxDecoration(
+                    color: Color(0xff345ea8),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                  child: TextButton(
+                    // group34456KJR (4:1128)
+                    onPressed: () {
+                      createSetting();
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                    ),
+                    child: Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      decoration: BoxDecoration(
+                        color: const Color(0xff4ab4de),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Center(
+                        child: Center(
+                          child: Text(
+                            'Simpan',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              height: 1.5,
+                              color: Color(0xffffffff),
+                            ),
                           ),
                         ),
                       ),
@@ -214,8 +344,8 @@ class _AdminSettingState extends State<AdminSetting> {
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
